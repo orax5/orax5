a22d7c5e72149a8973610ec03ffe3f1b014763d0
YounmiSon <symym629@gmail.com>
2023-01-13 16:22:49 +0900

    전역 스타일 파일 수정

--- client/styles/global-style.js
+++ client/styles/global-style.js
@@ -3,34 +3,36 @@ import styled, { createGlobalStyle } from "styled-components";
 // 전체 화면에 같은 여백을 주기 위한 설정
 export const PageContainer = styled.div`
   padding: 8rem 5rem 5rem 5rem;
+  /* width: 100vw;
+  height: auto;
+  background-color: red;
+  display: grid;
+  place-items: center;
+  place-content: center;
+  grid-template-columns:
+    "logo     .      menu"
+    "margin  main  margin"
+    "footer footer footer"; */
 `;
 
 // reset.css 설정 적용
 export const GlobalStyle = createGlobalStyle`
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed, 
-figure, figcaption, footer, header, hgroup, 
-menu, nav, output, ruby, section, summary,
-time, mark, audio, video{
-  // 배경 및 글자색 설정
-	background-color: black;
+
+@font-face {
+    font-family: 'Pretendard-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+    font-weight: 400;
+    font-style: normal;
+}
+
+*, html, body {
+	background-color: black; 
   color: white;
-  // 화면 기본 설정(여백/사이즈)
   margin: 0;
 	padding: 0;
 	border: 0;
   box-sizing: border-box;
-	font-size: 100%;
-	font: inherit;
-	vertical-align: baseline;
+  font-family: 'Pretendard-Regular';
   text-decoration: none; 
   max-width: 100vw;
   -ms-overflow-style: none;
@@ -40,26 +42,25 @@ time, mark, audio, video{
   display: none;
 }
 
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure, 
-footer, header, hgroup, menu, nav, section {
-	display: block;
+// 영역에 배경색 설정되는거 방지
+Image, img, div, span, p, h1, table, thead, tbody, tr, th,td, svg, strong, a, Link {
+  background-color: transparent;
 }
-ol, ul,li {
+ul,li {
+  background-color: transparent;
 	list-style: none;
 }
 table {
+  background-color: transparent;
 	border-collapse: collapse;
 	border-spacing: 0;
 }
 button {  
+  background-color: transparent;
   cursor: pointer;
-  color: inherit;
-  background-color: inherit;
 }
 input, select, textarea{
-  color: inherit;  
-  background-color: black;
+  background-color: transparent;
   box-shadow: none;
 }
 `;
