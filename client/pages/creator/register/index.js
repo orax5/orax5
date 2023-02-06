10b73800744665843dff296db5e9b9954b27471d
YounmiSon <symym629@gmail.com>
2023-02-06 10:31:55 +0900

    이미지 업로드 페이지 추가

--- client/pages/creator/register/index.js
+++ client/pages/creator/register/index.js
@@ -1,13 +1,32 @@
 import React from "react";
 import Link from "next/link";
+import styled from "styled-components";
+import Image from "next/image";
 
 const index = () => {
   return (
-    <div>
-        
-      <Link href="/creator/register/detailForm">detailForm</Link>
-    </div>
+    <MainContainer>
+      <div></div>
+      <MainArea>
+        <Image
+          src="/Img/sample.jpg"
+          alt="detail_page_image"
+          width={500}
+          height={500}
+        />
+        <input type="file" />
+        <button>
+          <Link href="/creator/register/detailForm">다음 단계로 이동</Link>
+        </button>
+      </MainArea>
+      <div></div>
+    </MainContainer>
   );
 };
-
+const MainContainer = styled.div`
+  ${(props) => props.theme.gridLayout.mainGrid};
+`;
+const MainArea = styled.div`
+  ${(props) => props.theme.align.flexCenterColumn};
+`;
 export default index;
