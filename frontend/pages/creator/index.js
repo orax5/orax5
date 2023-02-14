a50629ea569e34aad95a059c33241415084a9113
YounmiSon <symym629@gmail.com>
2023-02-14 21:42:21 +0900

    크리에이터 펀딩 신청 완료

--- frontend/pages/creator/index.js
+++ frontend/pages/creator/index.js
@@ -3,114 +3,16 @@ import styled from "styled-components";
 import Link from "next/Link";
 import { FaEthereum } from "react-icons/fa";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 const index = () => {
-  const datas = [
-    {
-      id : 1,name: "사진스힙합",
-      category: "힙합",
-      scale: "0.123",
-      targetPrice: "1000",
-      amount: "100개",
-      state: "반려",
-      dating: "14일",
-    },
-    {
-      id : 2,name: "나진스댄스",
-      category: "댄스",
-      scale: "0.456",
-      targetPrice: "1000",
-      state: "펀딩진행중",
-      dating: "7일",
-      amount: "100개",
-    },
-    {
-      id : 3,name: "다진스RnB",
-      category: "RnB",
-      scale: "0.789",
-      targetPrice: "1000",
-      state: "펀딩실패",
-      dating: "7일",
-      amount: "100개",
-    },
-    {
-      id : 4,name: "라진스발라드",
-      category: "발라드",
-      scale: "1.24",
-      state: "펀딩진행중",
-      dating: "14일",
-      targetPrice: "1000",
-      amount: "100개",
-    },
-    {
-      id : 5,name: "마진스팝",
-      category: "팝",
-      scale: "2.24",
-      state: "심사대기중",
-      dating: "7일",
-      targetPrice: "1000",
-      amount: "100개",
-    },
-    {
-      id : 6,
-      name: "뉴진스락",
-      category: "락",
-      scale: "4.44",
-      fundingState:3,
-      state: "펀딩성공",
-      dating: "14일",
-      targetPrice: "1000",
-      amount: "100개",
-    },
-    {
-      id : 7,name: "큐락비락",
-      category: "락",
-      scale: "1.44",
-      state: "펀딩진행중",
-      dating: "7일",
-      targetPrice: "1000",
-      amount: "100개",
-    },
-    {
-      id : 8,name: "블락비댄스",
-      category: "댄스",
-      scale: "2.44",
-      admin: 2,
-      state: "펀딩시작",
-      dating: "7일",
-      targetPrice: "1000",
-      amount: "100개",
-    },
-    {
-      id : 9,name: "비락비발라드",
-      category: "발라드",
-      scale: "10.44",
-      state: "펀딩진행중",
-      dating: "7일",
-      targetPrice: "1000",
-      amount: "100개",
-    },
-    {
-      id : 10,name: "블락비힙합",
-      category: "힙합",
-      scale: "0.44",
-      state: "펀딩실패",
-      dating: "7일",
-      targetPrice: "1000",
-      amount: "100개",
-    },
-  ];
-
-  const fund = [{id : 1, state : 1},{id :3, state : 1},{id :4, state : 1},{id : 9, state : 1},]
-
+  const [listdata, setListData] = useState([]);
   const [clipAccount, setClipAccount] = useState(false);
-  // 보여줄 페이지의 인덱스
-  const [index, setIndex] = useState(0);
 
   const Dtoken = useSelector((state) => state.user.contracts.Dtoken);
   const Ftoken = useSelector((state) => state.user.contracts.Ftoken);
   const ftokenCA = useSelector((state) => state.user.contracts.ftokenCA);
-  const account = useSelector((state) => state.user.users.user_wallet);
+  const account = useSelector((state) => state.user.contracts.account);
 
   // 클립보트 핸들러
   const copyClipBoardHandler = async (text) => {
@@ -123,7 +25,19 @@ const index = () => {
     } catch (e) {}
   };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios({
+      url: `http://localhost:3001/creator/mypage/${account}`,
+      method: "get",
+    })
+      .then((res) => {
+        const shinList = res.data;
+        setListData(shinList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const nickname = useSelector((state) => state.user.users.user_nickname);
   console.log(nickname);
@@ -142,7 +56,7 @@ const index = () => {
   };
 
   // 펀딩 성공시 크리에이터가 돈 받는 함수
-  const getFundingMoney = async() => {
+  const getFundingMoney = async () => {
     await Ftoken.isSuccessFundding(1);
     Ftoken.on("isSuccessFunddingEvent", (account, tokenId, value) => {
       console.log(account.toString());
@@ -159,7 +73,6 @@ const index = () => {
           <TitleArea>
             <h2>
               환영합니다!
-              <br />
               {nickname} 님
             </h2>
             <Link href="/creator/register">펀딩 신청</Link>
@@ -193,19 +106,24 @@ const index = () => {
                 </tr>
               </thead>
               <tbody>
-                {datas.map((data, idx) => (
+                {listdata.map((data, idx) => (
                   <tr key={idx}>
-                    <td>{data.category}</td>
-                    <td>{data.name}</td>
-                    <td>{data.amount}</td>
-                    <td>{data.targetPrice}{"ETH"}</td>
-                    <td>{data.dating}</td>
+                    <td>{data.shin_category}</td>
+                    <td>{data.shin_title}</td>
+                    <td>{data.shin_amount}</td>
                     <td>
-                      {data.state} 
+                      {data.shin_nft_totalbalance}
+                      {"ETH"}
                     </td>
+                    <td>{data.shin_period}</td>
+                    <td>{data.shin_ispermit == 1 ? "승인대기중" : ""}</td>
                     <td>
-                      {data.admin == 2 && <FundingStartBtn onClick={FundingMinting}>펀딩시작트랜잭션</FundingStartBtn>}
-                      {data.fundingState == 3 && <FundingStartBtn onClick={getFundingMoney}>펀딩성공돈받자</FundingStartBtn>}
+                      {data.shin_ispermit == 2 && (
+                        <FundingStartBtn onClick={FundingMinting}>펀딩시작트랜잭션</FundingStartBtn>
+                      )}
+                      {data.shin_ispermit == 3 && (
+                        <FundingStartBtn onClick={getFundingMoney}>펀딩성공돈받자</FundingStartBtn>
+                      )}
                     </td>
                   </tr>
                 ))}
@@ -249,7 +167,7 @@ const TitleArea = styled.div`
     ${(props) => props.theme.align.flexCenter};
     cursor: pointer;
     color: black;
-    width: 23.8rem;
+    width: 15rem;
     height: 4rem;
     background-color: white;
     border-radius: 0.5rem;
@@ -288,8 +206,7 @@ const CreatorAddress = styled.div`
 
 const FundingStartBtn = styled.button`
   color: white;
-  background:plum;
-`
-
+  background: plum;
+`;
 
 export default index;
