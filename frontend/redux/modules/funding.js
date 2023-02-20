c3d08d8f417dcdb74c5295be5e5d1ab7c580bb26
YounmiSon <symym629@gmail.com>
2023-02-21 02:17:26 +0900

    펀딩 신청 관련 수정

--- frontend/redux/modules/funding.js
+++ frontend/redux/modules/funding.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 import produce from "immer";
 
-const NFT_COVER = "funding/NFT_COVER";
 const BASE_URL = "http://localhost:3001";
-const METADATAS = "funding/METADATAS";
+
+const NFT_COVER = "funding/NFT_COVER";
+const FUND_DATA = "funding/FUND_DATA";
 
 // 이미지 전송해서 S3변환 주소 돌려받기
 export const uploadImage = (formData) => {
@@ -28,37 +29,30 @@ export const uploadImage = (formData) => {
       });
   };
 };
-export const adminPermit = (id) => {
+// 크리에이터가 펀딩 오픈하기
+export const openFunding = (id) => {
   return async (dispatch, getState) => {
-    // 승인하면 1) state 변경하고
     await axios({
-      url: `${BASE_URL}/admin/mypage/permit/${id}`,
+      url: `http://localhost:3001/openfunding/${id}`,
       method: "post",
-      data: { fundingID: id },
-    }).then((res) => {
-      console.log(res);
-      // 2) 메타데이터 생성해서 funding 테이블에 담는다
-      axios({
-        url: `http://localhost:3001/openfunding/${id}`,
-        method: "post",
-        data: { shinId: id },
-      })
-        .then((res) => {
-          // 얘네를 들고있다가 유저가 펀딩 시작하기 즉, 민팅할 때 인자로 넘겨줘야 한다 여기의 tokenId가 바로 그 tokenId
-          const data = res.data;
-          dispatch({ type: METADATAS, payload: data });
-        })
-        .catch((err) => {
-          console.log(err);
+      data: { shinId: id },
+    })
+      .then((res) => {
+        const data = { balance : res.data.balance, tokenId: res.data.tokenId, metaData: res.data.metaData };
+        console.log(res.data);
+        dispatch({
+          type: FUND_DATA,
+          payload: { data },
         });
-    });
-    console.log(id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 };
-
 const init = {
   imgURL: {},
-  metadata: { tokenId: 0, metaData: null },
+  funding: {},
 };
 
 export default function funding(state = init, action) {
@@ -66,11 +60,10 @@ export default function funding(state = init, action) {
   switch (type) {
     case NFT_COVER:
       return { ...state, imgURL: payload.data };
-    case METADATAS:
-      return produce(state, (draft) => {
-        draft.metadata.metaData = payload.metaData;
-        draft.metadata.tokenId = payload.tokenId;
-      });
+    case FUND_DATA:
+      console.log(payload);
+      return { ...state, funding: payload.data };
+
     default:
       return state;
   }
