b10d7eb7f058b0614f66d799c3d554e565c6dd2a
YounmiSon <symym629@gmail.com>
2023-02-22 11:02:38 +0900

    ca 수정

--- frontend/hooks/ajyContract.js
+++ frontend/hooks/ajyContract.js
@@ -5,27 +5,27 @@ import saleToken from "../contracts/SaleToken.json";
 import fundingToken from "../contracts/FunddingToken.json";
 
 const ajyContract = () => {
+  const [tokenData, setTokenData] = useState(null);
+  const dCA = "0xe8417547dae553834046b2f653c888cfb84f6fe0";
+  const fCA = "0x6b11cC61CA3fD01D18F61B3945fC6178e4Ad6F9a";
+  const sCA = "0x03EFc8cD336BFc5fea70E1de19c22aE001685f9c";
+  useEffect(() => {
+    console.log(dtsToken);
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-    const [tokenData,setTokenData] = useState(null);
-    
-    useEffect(() => {
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
-        
-        (async () => {
-            const { chainId } = await provider.getNetwork();
-            console.log(chainId);
-            console.log("#$#$#$#");
-             const data = {
-                Dtoken: new ethers.Contract(dtsToken.networks[chainId].address, dtsToken.abi, provider.getSigner()),
-                Ftoken: new ethers.Contract(fundingToken.networks[chainId].address, fundingToken.abi, provider.getSigner()),
-                Stoken: new ethers.Contract(saleToken.networks[chainId].address, saleToken.abi, provider.getSigner()),
-                dtokenCA: dtsToken.networks[chainId].address,
-                ftokenCA: fundingToken.networks[chainId].address,
-                stokenCA: saleToken.networks[chainId].address,
-            };
-            setTokenData(data);
-        })();
-    },[])
+    (async () => {
+      const { chainId } = await provider.getNetwork();
+      const data = {
+        Dtoken: new ethers.Contract(dCA, dtsToken.abi, provider.getSigner()),
+        Ftoken: new ethers.Contract(fCA, fundingToken.abi, provider.getSigner()),
+        Stoken: new ethers.Contract(sCA, saleToken.abi, provider.getSigner()),
+        dtokenCA: dCA,
+        ftokenCA: fCA,
+        stokenCA: sCA,
+      };
+      setTokenData(data);
+    })();
+  }, []);
 
   return tokenData;
 };
